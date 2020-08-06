516e81866842879e2fd8ca3e59eda959c9cc1b63
itulupov1 <itulupov1@yandex.ru>
2020-08-06 23:01:40 +0700

    Задание

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert('hellos!');
+let money = 0;
+let income = 0;
+let addExpenses = 0;
+let deposit = 0;
+let mission = 0;
+let period = 0;
+let say = 'Hello, world';
+
+alert(say);
+
+console.log('Любой текст в консоль');
