329a4b45b14c1ca58b34694621e5e755e0ab547d
itulupov1 <itulupov1@yandex.ru>
2020-08-07 13:39:57 +0700

    lesson02

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-let money = 0;
-let income = 0;
-let addExpenses = 0;
-let deposit = 0;
-let mission = 0;
-let period = 0;
-let say = 'Hello, world';
+let money = 30000;
+let income = 'фриланс';
+let addExpenses = 'Квартплата, интернет, автомобиль, кафе';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
 
-alert(say);
 
-console.log('Любой текст в консоль');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен', period + ' месяцев');
+console.log('Цель заработать', mission + ' рублей');
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(', ');
+console.log(addExpenses);
+
+let budgetDay;
+budgetDay = money/30;
+console.log(budgetDay);
