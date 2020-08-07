c5d9a6ca264e99a01d12a3a6358ba9345514a408
itulupov1 <itulupov1@yandex.ru>
2020-08-07 15:47:11 +0700

    fix(js): error correction

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const budgetDay = money / 30;
 
 console.log(typeof money, typeof income, typeof deposit);
 
-console.log(addExpenses.lsength);
+console.log(addExpenses.length);
 console.log('Период равен', period + ' месяцев');
 console.log('Цель заработать', mission + ' рублей');
 
